53355a3ccb8b6b08ac57cb5b615066ae679c710c
jmorauga <jafet.mora.ugalde@intel.com>
2021-08-30 21:49:36 -0600

    Add general configurations to the app.

--- config.py
+++ config.py
@@ -5,13 +5,17 @@ basedir = os.path.abspath(os.path.dirname(__name__))
 
 
 class Config(object):
-    SECRET_KEY = 'P0rtf0l10@pp'
-    # SECRET_KEY = os.environ.get('PORTFOLIO_SECRET_KEY') # P0rtf0l10@pp
+    SECRET_KEY = os.environ.get('PORTFOLIO_SECRET_KEY') 
+    FLASK_SECRET = SECRET_KEY
+    JWT_SECRET_KEY = SECRET_KEY
+    DB_HOST = os.environ.get('PORTFOLIO_DB_HOST')
 
 
 class DevConfig(Config):
     DEBUG = True
+    DEVELOPMENT = True
 
 
 class ProdConfig(Config):
     DEBUG = False
+    DEVELOPMENT = False
