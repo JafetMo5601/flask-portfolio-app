44a6436d305ddb585cc7fe4ddfe0358c223e213a
jmorauga <jafet.mora.ugalde@intel.com>
2021-08-30 21:50:40 -0600

    Register the blueprints. Add the JWT Extension.

--- src/app.py
+++ src/app.py
@@ -1,5 +1,38 @@
+from flask_jwt_extended import JWTManager
+from pymongo import MongoClient
 from flask import Flask
 import argparse
+
+
+def connection():
+    # client = MongoClient('mongodb://' + os.environ.get('PORTFOLIO_DB_HOST') + ':27017/')
+    client = MongoClient('mongodb://localhost:27017/')
+    
+    db = client['portfolio']    
+    return db
+
+
+def get_collection(collection_name):
+    user = connection()[collection_name]
+    return user
+
+
+def create_api(config):
+    app = Flask(__name__)
+    JWTManager(app)
+    
+    app.config.from_object(config)
+    
+    from src.blueprints.admin import admin_bp
+    app.register_blueprint(admin_bp)
+    
+    from src.blueprints.auth import auth_bp
+    app.register_blueprint(auth_bp)
+    
+    from src.blueprints.info import info_bp
+    app.register_blueprint(info_bp)
+
+    return app
 
 
 def parse_args():
@@ -11,13 +44,3 @@ def parse_args():
         help='Flag for production deployment. Warning: This will disable debugging mode.'
     )
     return parser.parse_args()
-
-
-
-def create_api(config):
-    app = Flask(__name__)
-    app.config.from_object(config)
-    
-    # import blueprint and register them
-
-    return app
