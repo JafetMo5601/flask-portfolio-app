01cb34c41129d1e61e0989a5c0e63d5a4c621d67
JafetMo5601 <jafet.moraugalde15@gmail.com>
2021-09-02 22:53:12 -0600

    Add a PoC of the route to add new projects to the DB.

--- src/blueprints/admin/routes.py
+++ src/blueprints/admin/routes.py
@@ -1,12 +1,22 @@
 from flask_jwt_extended import jwt_required
-from flask import jsonify
+from flask import jsonify, request
 
 from . import admin_bp
-
-
-@admin_bp.route('/admin/', methods=['GET'])
+    
+    
+@admin_bp.route('/add-project/', methods=['POST'])
 @jwt_required()
-def admin():
+def add_project():
+    
+    project_info = request.json
+    #Update the DB
+    
     return jsonify(
-        message='This is the Admin Blueprint'
+        response = 'The project info is the following:\n'
+        + 'Title: ' + project_info['title'] + '\n' 
+        + 'Description: ' + project_info['description'] + '\n' 
+        + 'Image_path: ' + project_info['image'] + '\n' 
+        + 'Tools: ' + ' '.join(project_info['tools']) + '\n' 
     )
+    
+
