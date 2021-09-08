560f7b8435cd48ef7ca67dffd3bb17fcc24f9ab0
JafetMo5601 <jafet.moraugalde15@gmail.com>
2021-09-07 21:57:29 -0600

    Add the year attribute to the awards model.

--- src/models/awards.py
+++ src/models/awards.py
@@ -8,4 +8,4 @@ class Award(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(100), nullable=False)
-    # Add year
+    year = db.Column(db.String(4), nullable=False)
