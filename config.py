13a8c0f704452f82991c3acd2fedb827ac93bb59
JafetMo5601 <jafet.moraugalde15@gmail.com>
2021-09-10 22:14:02 -0600

    correct the files storage path of the app config.

--- config.py
+++ config.py
@@ -1,17 +1,14 @@
 import os
 
 
-basedir = os.path.abspath(os.path.dirname(__name__))
-
-
 class Config(object):
     SECRET_KEY = os.environ.get('PORTFOLIO_SECRET_KEY') 
     FLASK_SECRET = SECRET_KEY
     JWT_SECRET_KEY = SECRET_KEY
     SQLALCHEMY_DATABASE_URI = 'sqlite:///portfolio.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    UPLOAD_FOLDER = os.getcwd() + '\\src\\files_storage\\'
-
+    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'src\\files_storage\\')
+    
 class DevConfig(Config):
     DEBUG = True
     DEVELOPMENT = True
