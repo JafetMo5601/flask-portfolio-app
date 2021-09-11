6db90aa8cb21db4a4d576bead7c5c4556b3d0f74
JafetMo5601 <jafet.moraugalde15@gmail.com>
2021-09-10 22:18:16 -0600

    Adding try exceptions to avoid the falling of the API unexpectedly.

--- src/blueprints/info/info.py
+++ src/blueprints/info/info.py
@@ -9,49 +9,51 @@ from src import db
 
 def get_info():
     if Info.query.filter(Info.id == 1).first() is not None:
-        response = [dict(list((info.__dict__).items())[1:]) for info in Info.query.all()]
+        response = [dict(list((info.__dict__).items())[1:])
+                    for info in Info.query.all()]
         remove_files(response[0])
         return response[0], 200
-    else: 
-        return 'Info not found.', 404
+    else:
+        return 'Not found.', 404
 
 
 def get_projects():
-    try: 
-        response = [dict(list((project.__dict__).items())[1:]) for project in Project.query.all()]
+    try:
+        response = [dict(list((project.__dict__).items())[1:])
+                    for project in Project.query.all()]
         return response, 200
-    except: 
+    except:
         return 'There was an internal server error.', 500
-    
+
 
 def get_awards():
-    try: 
-        response = [dict(list((award.__dict__).items())[1:]) for award in Award.query.all()]
+    try:
+        response = [dict(list((award.__dict__).items())[1:])
+                    for award in Award.query.all()]
         return response, 200
-    except: 
-        return 'There was an internal server error.', 500   
-    
-    
+    except:
+        return 'There was an internal server error.', 500
+
+
 def get_resume():
-    return send_from_directory(current_app.config['UPLOAD_FOLDER'] + 'resume\\', 'resume.pdf')
+    try:
+        return send_from_directory(current_app.config['UPLOAD_FOLDER'] + 'resume\\', 'resume.pdf'), 200
+    except:
+        return 'Not found.', 404
 
 
-def get_cover():
-    cover_path = current_app.config['UPLOAD_FOLDER'] + 'images\\info\\'
-    cover_name = os.listdir(cover_path)[0]
-    print(cover_name)
-    return send_from_directory(cover_path, cover_name)
-
-
-def get_photo():
-    photo_path = current_app.config['UPLOAD_FOLDER'] + 'images\\info\\'
-    photo_name = os.listdir(photo_path)[1]
-    print(photo_name)
-    return send_from_directory(photo_path, photo_name)
+def get_image(image_name, image_type):
+    try:
+        image_path = os.path.join(
+            current_app.config['UPLOAD_FOLDER'], ('images\\' + image_type))
+        image_name = [name for name in os.listdir(
+            image_path) if image_name.replace(' ', '_') in name][0]
+        return send_from_directory(image_path, image_name), 200
+    except:
+        return 'Bad request.', 400
 
 
 def remove_files(response):
     del response['resume_path']
     del response['cover']
     del response['photo']
-    
