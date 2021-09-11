1576d2d5a377da701697c5e69bfdd14797c2ce4c
Jafet Mora Ugalde <jafet.mora.ugalde@intel.com>
2021-09-10 22:44:56 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,14 +6,14 @@ from src import create_api, parse_args
 def main(enable_prod: bool):    
     if enable_prod: 
         host = '0.0.0.0'
-        port = 5900
+#         port = 5900
         api = create_api(ProdConfig)
     else: 
         host = '127.0.0.1'
-        port = 3000
+#         port = 3000
         api = create_api(DevConfig)
         
-    api.run(host=host, port=port)
+    api.run(host=host)
         
 
 if __name__ == '__main__':
