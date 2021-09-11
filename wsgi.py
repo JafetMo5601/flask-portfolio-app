6b0b6a4d1018eb1e20a66ffa0e57c2c5a3a721ad
jmo.py <33186474+JafetMo5601@users.noreply.github.com>
2021-09-11 10:36:05 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,9 +5,12 @@ from src import create_api
 
 if (os.getenv('ENVIRONMENT', 'prod') == 'env'): 
     api = create_api(DevConfig)
+    host = '127.0.0.1'
 else: 
     api = create_api(ProdConfig)
+    host = '0.0.0.0'
 
     
 if __name__ == '__main__':
-    api.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host=host, port=port)
