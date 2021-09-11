f3290580d0d45124956c183352c6959a0899822e
jmo.py <33186474+JafetMo5601@users.noreply.github.com>
2021-09-11 10:31:06 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ import os
 from config import DevConfig, ProdConfig
 from src import create_api, parse_args
 
-if (os.environ['ENVIRONMENT'] == 'env'): 
+if (os.getenv('ENVIRONMENT', 'prod') == 'env'): 
     api = create_api(DevConfig)
 else: 
     api = create_api(ProdConfig)
