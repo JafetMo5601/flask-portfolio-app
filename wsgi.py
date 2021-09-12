bf72ef9d26054829657f2937471164c0dfe80187
JafetMo5601 <jafet.moraugalde15@gmail.com>
2021-09-11 18:42:50 -0600

    Change the text 'env' to 'dev' in the if condition since env won't be used.

--- wsgi.py
+++ wsgi.py
@@ -3,14 +3,15 @@ import os
 from config import DevConfig, ProdConfig
 from src import create_api
 
-if (os.getenv('ENVIRONMENT', 'prod') == 'env'): 
+
+if (os.getenv('ENVIRONMENT', 'prod') == 'dev'):
     api = create_api(DevConfig)
     host = '127.0.0.1'
-else: 
+else:
     api = create_api(ProdConfig)
     host = '0.0.0.0'
 
-    
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     api.run(host=host, port=port)
